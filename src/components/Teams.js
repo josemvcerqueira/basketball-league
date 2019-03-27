5742e4c5cc40fd1989038e45ad1de62e11a7202f
Jose Cerqueira <jose@origensmedia.com>
2019-03-27 16:49:01 +0000

    feat: added the sidebar component to the Teams class

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import { getTeamNames } from "../api";
 
 class Teams extends Component {
+	state = {
+		teamNames: [],
+		loading: true
+	};
+
+	componentDidMount() {
+		this.fetchTeamNames();
+	}
+
+	fetchTeamNames = async () => {
+		const teamNames = await getTeamNames();
+		this.setState(() => ({
+			loading: false,
+			teamNames
+		}));
+	};
+
 	render() {
-		return <div className="container two-column"> Teams</div>;
+		const { loading, teamNames } = this.state;
+		const { location, match } = this.props;
+		return (
+			<div className="container two-column">
+				<Sidebar
+					loading={loading}
+					title="Teams"
+					list={teamNames}
+					{...this.props}
+				/>
+
+				{loading === false && location.pathname === "/teams" ? (
+					<div className="sidebar-instruction">Select a team</div>
+				) : null}
+			</div>
+		);
 	}
 }
 
