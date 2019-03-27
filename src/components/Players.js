2a0c6a47a53341470a5177f9e59004539510aef4
Jose Cerqueira <jose@origensmedia.com>
2019-03-27 16:30:18 +0000

    Constructed the player card inside the Player component.
    The card renders all the information regarding a player in a new link.
    And has a Link component to the team's page.

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import slug from "slug";
 import Sidebar from "./Sidebar";
 import { getPlayers } from "../api";
 import { parse } from "query-string";
@@ -25,8 +27,7 @@ class Players extends Component {
 	};
 	render() {
 		const { players, loading } = this.state;
-		const { location } = this.props;
-		console.log(location);
+		const { location, match } = this.props;
 		return (
 			<div className="container two-column">
 				<Sidebar
@@ -38,6 +39,74 @@ class Players extends Component {
 				{loading === false && location.pathname === "/players" ? (
 					<div className="sidebar-instruction">Select a Player</div>
 				) : null}
+
+				<Route
+					path={`${match.url}/:playerId`}
+					render={({ match }) => {
+						if (loading === true) return null;
+						const {
+							name,
+							position,
+							teamId,
+							number,
+							avatar,
+							apg,
+							ppg,
+							rpg,
+							spg
+						} = players.find(
+							player =>
+								slug(player.name) === match.params.playerId
+						);
+						return (
+							<div className="panel">
+								<img
+									className="avatar"
+									src={`${avatar}`}
+									alt={`${name}'s avatar`}
+								/>
+								<h1 className="medium-header">{name}</h1>
+								<h3 className="header">#{number}</h3>
+								<div className="row">
+									<ul
+										className="info-list"
+										style={{ marginRight: 80 }}
+									>
+										<li>
+											Team
+											<div>
+												<Link
+													style={{ color: "#68809a" }}
+													to={`/${teamId}`}
+												>
+													{teamId[0].toUpperCase() +
+														teamId.slice(1)}
+												</Link>
+											</div>
+										</li>
+										<li>
+											Position<div>{position}</div>
+										</li>
+										<li>
+											PPG<div>{ppg}</div>
+										</li>
+									</ul>
+									<ul className="info-list">
+										<li>
+											APG<div>{apg}</div>
+										</li>
+										<li>
+											SPG<div>{spg}</div>
+										</li>
+										<li>
+											RPG<div>{rpg}</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+						);
+					}}
+				/>
 			</div>
 		);
 	}
