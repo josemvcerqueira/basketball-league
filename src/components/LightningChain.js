9b4da5d11c77be03bb095b0264da6443bbf9f297
Jose Cerqueira <jose@origensmedia.com>
2019-03-28 23:47:23 +0000

    Feat: Implemented react-transition-group to Players.js file

---
+++ src/components/LightningChain.js
@@ -0,0 +1,28 @@
+const lightningHash = data => data + "*";
+
+class Block {
+	constructor(data, hash, lastHash) {
+		this.data = data;
+		this.hash = hash;
+		this.lastHash = lastHash;
+	}
+}
+
+class Blockchain {
+	constructor() {
+		const genesis = new Block("gen-data", "gen-hash", "gen-lastHash");
+		this.chain = [genesis];
+	}
+
+	addBlock(data) {
+		const lastHash = this.chain[this.chain.length - 1].hash;
+		const hash = lightningHash(data + lastHash);
+		const block = new Block(data, hash, lastHash);
+		this.chain.push(block);
+	}
+}
+
+const fooBlockchain = new Blockchain();
+fooBlockchain.addBlock("one");
+fooBlockchain.addBlock("Two");
+fooBlockchain.addBlock("Three");
